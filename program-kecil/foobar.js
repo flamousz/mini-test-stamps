f73cb7a82047c140977e2da366c088be8867f67b
flamousz <valflamous@gmail.com>
2023-09-17 03:01:19 +0700

    finishing test

---
+++ program-kecil/foobar.js
@@ -0,0 +1,38 @@
+const fooBar = () => {
+	let result = [];
+
+	// Function to check if a number is prime
+	const isPrime = (n) => {
+		if (n <= 1) {
+			return false;
+		}
+
+		for (let i = 2; i * i <= n; i++) {
+			if (n % i === 0) {
+				return false;
+			}
+		}
+
+		return true;
+	};
+
+	for (let i = 100; i > 0; i--) {
+		if (!isPrime(i)) {
+			if (i % 5 === 0 && i % 3 === 0) {
+				result.push("FooBar");
+			} else if (i % 5 === 0) {
+				result.push("Bar");
+			} else if (i % 3 === 0) {
+				result.push("Foo");
+			} else {
+				result.push(i);
+			}
+		}
+	}
+    
+
+
+	return result.join(', ');
+};
+
+console.log(fooBar());
